43c193686c03be6c30ab8ebb5ab860888802171d
Tran Tu <tu.tx225677@sis.hust.edu.vn>
2025-04-28 22:21:35 +0700

    first commit

---
+++ backend/config/database.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require('sequelize');
+
+// Khởi tạo đối tượng Sequelize với thông tin kết nối cụ thể
+const sequelize = new Sequelize('WebPL', 'postgres', 'kai2604', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+// Xuất đối tượng sequelize để sử dụng ở những nơi khác trong ứng dụng
+module.exports = sequelize;
